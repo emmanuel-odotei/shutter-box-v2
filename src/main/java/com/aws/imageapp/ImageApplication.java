2dd5ac292cf4c6afe59ce5a6e47f328de34ac1f9
Emmanuel Odoi Odotei <99666388+emmanuel-odotei@users.noreply.github.com>
2025-06-18 15:23:17 +0000

    application files added

---
+++ src/main/java/com/aws/imageapp/ImageApplication.java
@@ -0,0 +1,13 @@
+package com.aws.imageapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run( ImageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/imageapp/ImageApplicationTests.java
@@ -0,0 +1,14 @@
+package com.aws.imageapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println( "true = " + true );
+	}
+
+}
